5b0c8d3812402abffa90b1688cfc67bd2b714cde
Sean <e0406739@u.nus.edu>
2020-12-27 04:31:00 +0800

    comment unused

--- src/Header/Search/SearchField/SearchField.js
+++ src/Header/Search/SearchField/SearchField.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-use-before-define */
-import React, { useState } from "react";
+import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import "./searchField.css";
 
-const users = ["Sean", "John", "Peter", "Abby", "Jacob", "Ashley", "Dennis"];
+// const users = ["Sean", "John", "Peter", "Abby", "Jacob", "Ashley", "Dennis"];
 
 const SearchField = () => {
-  const [value, setValue] = useState(null);
+  // const [value, setValue] = useState(null);
   return (
     <div className="searchField">
       <SearchIcon />
